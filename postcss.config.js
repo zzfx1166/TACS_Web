583f85fd332da3f6151f1655a1e2f3b8b431e3ee
Redapple_one <3134641406@qq.com>
2024-10-24 13:55:41 +0000

    微调 增加了browserlist

--- postcss.config.js
+++ postcss.config.js
@@ -16,11 +16,3 @@ module.exports = {
       require('postcss-preset-env'),
   ],
 };
-module.exports = (ctx) => ({
-  map: ctx.options.map,
-  parser: ctx.options.parser,
-  plugins: {
-    'postcss-import': { root: ctx.file.dirname },
-    cssnano: ctx.env === 'production' ? {} : false,
-  },
-})
